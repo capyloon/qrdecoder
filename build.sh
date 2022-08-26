2d1345671e77f5e5193ec6071d0cce240e933b8b
Fabrice Desré <fabrice@desre.org>
2022-08-26 10:42:25 -0700

    Generate smaller code by combining wit-bindgen, wasm-bindgen and wasm-opt

--- build.sh
+++ build.sh
@@ -9,9 +9,17 @@ mv *.js static/
 
 cargo build --release --target=wasm32-unknown-unknown
 
-wasm-opt target/wasm32-unknown-unknown/release/qrdecoder.wasm \
-    -O4 -o static/qrdecoder.wasm \
-    --remove-imports
+wasm-bindgen \
+    --target web \
+    --out-dir static \
+    target/wasm32-unknown-unknown/release/qrdecoder.wasm
+
+wasm-opt static/qrdecoder_bg.wasm \
+    -O4 -o static/qrdecoder.wasm
+
+rm static/*.d.ts
+rm static/qrdecoder.js # wasm-bindgen generated that we don't need.
+rm static/qrdecoder_bg.wasm
 
 mv static/qrdecoder-module.js static/qrdecoder_module.js
 cp static/* ../nutria/apps/camera/qrdecoder/
